95acb7b3aa9940ff12fe41f3dd7f6a6a81b00a58
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-08-15 16:46:21 -0300

    operadores 2

--- variaveis/tipos-variaveis/src/TiposVariavies.java
+++ variaveis/tipos-variaveis/src/TiposVariavies.java
@@ -6,6 +6,14 @@ public class TiposVariavies {
         short numeroCurto = 1;
         int numeroNormal = numeroCurto; 
         short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5; 
+
+        numero = 10; 
+
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14;
     }
     
 }
