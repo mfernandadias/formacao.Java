6cf56fcb9709f3bec26e5440ac085cda4bcdab81
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-08-15 16:00:38 -0300

    linguagem tipada

--- variaveis/tipos-variaveis/src/TiposVariavies.java
+++ variaveis/tipos-variaveis/src/TiposVariavies.java
@@ -1,3 +1,11 @@
 public class TiposVariavies {
+
+    public static void main(String[ ] args) throws Exception {
+        double salariominimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto; 
+        short numeroCurto2 = (short) numeroNormal;
+    }
     
 }
