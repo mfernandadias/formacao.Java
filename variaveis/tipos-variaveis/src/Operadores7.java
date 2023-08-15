acece43b2f1202e19df8b7d95373418a7b03ae5b
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-08-15 17:49:27 -0300

    operadores 7

---
+++ variaveis/tipos-variaveis/src/Operadores7.java
@@ -0,0 +1,15 @@
+public class Operadores7 {
+    public static void main (String[ ] args) {
+        boolean condicao1 = true; 
+
+        boolean condicao2 = false; 
+
+        if(condicao1 && condicao2) {
+            System.out.println("as duas condições são verdadeiras");
+        }
+
+        if(condicao1 || condicao2) {
+            System.out.println("Uma das condiçõs é verdadeira");
+        }
+    }
+}
