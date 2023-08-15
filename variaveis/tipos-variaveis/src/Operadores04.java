864cda7d9b5095cbe778ed553e460ab99601a87e
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-08-15 17:03:58 -0300

    operadores 4

---
+++ variaveis/tipos-variaveis/src/Operadores04.java
@@ -0,0 +1,16 @@
+public class Operadores04 {
+    public static void main(String[] args) {
+       /*  int numero = 5; 
+        //x repetição
+        numero = numero + 1; 
+        //numero = numeoro + 1;
+
+        System.out.println(numero); */
+
+        boolean variavel = true; 
+
+        variavel = !variavel; 
+
+        System.out.println(variavel);
+    }
+}
