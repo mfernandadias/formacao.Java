f2b3106743bfa4d303fc8c76a173330ceae6471d
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-08-15 15:04:41 -0300

    métodos em Java

--- anatomiadeclasses/src/MinhaClasse.java
+++ anatomiadeclasses/src/MinhaClasse.java
@@ -1,6 +1,16 @@
 public class MinhaClasse {
 
     public static void main (String [] args) {
-        System.out.print ("olá turma, sejam bem-vindos");
+        String primeiroNome = "Gleyson";
+        String segundoNome = "Sampaio";
+
+        String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return primeiroNome.concat("").concat(segundoNome);
     }
 }
