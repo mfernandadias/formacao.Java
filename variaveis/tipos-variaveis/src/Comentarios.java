df9e389e2ce75ee521ceac63807126b4cea5dbb1
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-08-15 22:56:36 -0300

    comentarios

---
+++ variaveis/tipos-variaveis/src/Comentarios.java
@@ -0,0 +1,17 @@
+public class Comentarios {
+    public static void main(String [] args) {
+        //olá eu sou um comentário em uma única linha 
+        /* olá 
+        sou um comentario 
+        que posso ser mais detalhado 
+        quando necessário
+         */
+    }
+     //olá eu sou um comentário em uma única linha 
+        /* olá 
+        sou um comentario 
+        que posso ser mais detalhado 
+        quando necessário
+         */
+        
+}
