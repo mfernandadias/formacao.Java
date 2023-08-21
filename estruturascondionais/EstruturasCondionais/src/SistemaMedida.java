ffcaea5fec0b477f4e33bd0c54fd3e92b4629961
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-08-21 17:55:12 -0300

    sistema de medida

---
+++ estruturascondionais/EstruturasCondionais/src/SistemaMedida.java
@@ -0,0 +1,17 @@
+public class SistemaMedida {
+    
+    public static void mai(String[] args) {
+        string sigla = "M";
+
+        if(sigla == "p")
+             System.out.println("PEQUENO");
+        else if(sigla == "M")
+            System.out.println("MEDIA");
+        else if(sigla == "G")
+            System.out.println("GRANDE");
+        else 
+           System.out.println("INDEFINIDO");
+
+
+    }
+}
