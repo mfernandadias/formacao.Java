73540f98dbf6a5b7193f2d1a5646fadbdcc4111f
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-08-22 11:40:43 -0300

    for com arrays

---
+++ estruturascondionais/EstruturasCondionais/src/ExemploForArrays.java
@@ -0,0 +1,10 @@
+public class ExemploForArrays {
+    
+    public static void main(String[] args) {
+        String alunos [] = {"FELIPE", "Jonas", "JULIA", "Marcos"};
+
+        for (int x =0; X<alunos.length; x++){
+            System.out.pritln("O aluno no indice x=" + x + " é "  + alunos[x]);
+        }
+    }
+}
