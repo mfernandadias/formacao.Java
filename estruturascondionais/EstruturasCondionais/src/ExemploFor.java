4e1d32929c20d4061d6e00ed5dfc52a92011368f
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-08-22 11:33:24 -0300

    exemplo de for

---
+++ estruturascondionais/EstruturasCondionais/src/ExemploFor.java
@@ -0,0 +1,9 @@
+public class ExemploFor {
+
+    public static void main(String[] args) {
+        for (int carneirinhos = 1; carneironhos <= 20; carneirinhos ++ ) {
+            System.out.println("Contando Carneirinhos " + carneirinhos);
+        }
+        Systemm.out.println("Joãozinho Dormiu");
+    }
+}
