e1cb572ca7736d2bc0fe61f92c409ec865143ece
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2023-09-25 20:35:57 -0300

    exercício float

---
+++ variaveis/tipos-variaveis/src/Geeks.java
@@ -0,0 +1,16 @@
+class Geeks{
+
+    static void printInFormat(float a, float b){
+        float result = a / b;
+
+        // Para imprimir até 3 casas decimais
+        System.out.printf("%.3f", result );
+    }
+
+    public static void main(String[] args) {
+        float a = 5.43;
+        float b = 2.653;
+
+       // printInFormat(a, b);
+    }
+}
