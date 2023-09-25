89544f96582d8eaa2a9ac0065f5c2b3d55b831f6
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2023-09-25 18:52:05 -0300

    string

---
+++ variaveis/tipos-variaveis/src/string.java
@@ -0,0 +1,7 @@
+/*public class string {
+    //Declara String sem usar o operador new
+    String s = "GeeksforGeeks";
+
+    //Declara String usando o novo operador
+    //String s1 = new String("GeeksforGeeks");
+} */
