44533d73bf36aa06f365dfe9aed3606cfdf8f4c9
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2023-09-25 18:12:02 -0300

    hello word

--- HelloGithub/src/br/com/mariafernanda/git/HelloGit.java
+++ HelloGithub/src/br/com/mariafernanda/git/HelloGit.java
@@ -3,7 +3,7 @@ package br.com.mariafernanda.git;
 public class HelloGit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 		System.out.println("Heloo, wordl");
 	}
 
