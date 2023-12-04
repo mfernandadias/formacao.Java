dcf1c3aaa457d2ca0659b867c8f443d0956a9aad
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2023-12-04 20:26:53 -0300

    prorjeto variáveis

---
+++ primeiroProjeto/src/Variaveis.java
@@ -0,0 +1,12 @@
+public class Variaveis {
+    public static void main(String [] args) {
+        String varavelA = "Hello ---- Word";
+        int variavelInt = 1;
+        double variaDouble = 1.1;
+
+        System.out.println(variavelA);
+        System.out.println(variavelInt);
+
+    }
+
+}
