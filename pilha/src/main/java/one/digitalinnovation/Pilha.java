1caa2a858899ba941592c92ddbb2a8a92d980f2d
mfernandasdias <91575360+mfernandadias@users.noreply.github.com>
2023-12-19 22:25:19 -0300

    pilha

--- pilha/src/main/java/one/digitalinnovation/Pilha.java
+++ pilha/src/main/java/one/digitalinnovation/Pilha.java
@@ -2,5 +2,21 @@ package one.digitalinnovation;
 
 public class Pilha {
 
-    private No refNo;
+    private No refNoEntradadaPilha;
+
+    public Pilha() {
+        this.refNoEntradadaPilha = null;
+    }
+    
+    public No top(){
+
+    }
+    public boolean isEmpty(){
+        if(refNoEntradaPilha == null ){
+            return true;
+        }
+        return false;
+        return refNoEntradaPilha == null ? true : false;
+    }
+
 }
